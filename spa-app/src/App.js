e5c691e160db71fca6d09729b3649f41d3a01c13
Zhanna <zhannaavanesova@yahoo.com>
2021-05-30 19:41:09 -0600

    router changes

--- spa-app/src/App.js
+++ spa-app/src/App.js
@@ -2,7 +2,8 @@ import React from 'react';
 import './App.css';
 import { Navbar, Nav, NavDropdown } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+// import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import {HashRouter as Router, Switch, Route, Link} from 'react-router-dom';
 import About from './components/About';
 import Contact from './components/Contact';
 import services from './components/services';
@@ -22,19 +23,24 @@ function App() {
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="mr-auto">
-            <Nav.Link href="about-us">About Us</Nav.Link>
-            <Nav.Link href="contact">Contact</Nav.Link>
-            <Nav.Link href="services">Services</Nav.Link>
+            <Nav.Link as={Link} to="/about-us" href="about-us">About Us</Nav.Link>
+            <Nav.Link as={Link} to="/contact"href="contact">Contact</Nav.Link>
+            <Nav.Link as={Link} to="/services"href="services" href="services">Services</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Navbar>
-      <Switch>
+      <Route path='/contact' exact component={Contact}></Route>
+      <Route path='/services' exact component={services}></Route>
+      <Route path='/' exact component={About}></Route>
+      <Route path='/home' exact component={About}></Route>
+      <Route path='/about-us' exact component={About}></Route>
+      {/* <Switch>
         <Route path='/about-us' exact component={About}></Route>
         <Route path='/contact' exact component={Contact}></Route>
         <Route path='/services' exact component={services}></Route>
         <Route path='/' exact component={About}></Route>
         <Route path='/home' exact component={About}></Route>
-      </Switch>
+      </Switch> */}
       </Router>
       </header>
     </div>
